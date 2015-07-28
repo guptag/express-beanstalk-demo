eea6a96ad35cf471639b55f92899bf455e269bb1
Gupta Garuda <g.garuda@samsung.com>
2015-07-27 17:13:25 -0700

    config changes

--- server/deploy/prod/prod.sh
+++ server/deploy/prod/prod.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
-cp .ebextensions/* $SCRIPT_DIR/../../.ebextensions/
-cp .elasticbeanstalk/* $SCRIPT_DIR/../../.elasticbeanstalk/
+cp .ebext/* $SCRIPT_DIR/../../.ebextensions/
+cp .ebconfig/* $SCRIPT_DIR/../../.elasticbeanstalk/
 
 cd $SCRIPT_DIR/../..
 eb use prod-env
